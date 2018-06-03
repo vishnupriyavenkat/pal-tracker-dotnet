noizwaves/pal-tracker-dotnet-1

Persist time entries in database

test/PalTrackerTests/TimeEntryIntegrationTest.cs
@@ -17,6 +17,9 @@ namespace PalTrackerTests
 
         public TimeEntryIntegrationTest()
         {
+            Environment.SetEnvironmentVariable("VCAP_SERVICES", DbTestSupport.TestDbVcapJson);
+            DbTestSupport.ExecuteSql("TRUNCATE TABLE time_entries");
+
             _testClient = IntegrationTestServer.Start().CreateClient();
         }
 

vishnupriyavenkat/pal-tracker-dotnet

test/PalTrackerTests/TimeEntryIntegrationTest.cs
@@ -17,6 +17,9 @@ namespace PalTrackerTests
 
         public TimeEntryIntegrationTest()
         {
+            Environment.SetEnvironmentVariable("MYSQL__CLIENT__CONNECTIONSTRING", DbTestSupport.TestDbConnectionString);
+            DbTestSupport.ExecuteSql("TRUNCATE TABLE time_entries");
+
             _testClient = IntegrationTestServer.Start().CreateClient();
         }
 